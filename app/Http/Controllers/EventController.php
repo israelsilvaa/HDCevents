18af7b0f6704ec2eeb62bd3b6b64af92d381ff01
israel silva <Israel524.is@gmail.com>
2023-01-15 11:35:03 -0300

    #aula30 - Removendo presença do evento.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -62,9 +62,21 @@ class EventController extends Controller
 
         $event = Events::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+        if($user){
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = True;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard() {
@@ -129,8 +141,18 @@ class EventController extends Controller
 
         $event = Events::findOrFail($id);
         
-        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+        return redirect('/events/ '. $id)->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
 
+    public function leaveEvent($id) {
+        
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Events::findOrFail($id);
+        
+        return redirect('/dashboard')->with('msg', 'Você saiu do evento ' . $event->title . 'com sucesso!');
     }
 
 }
